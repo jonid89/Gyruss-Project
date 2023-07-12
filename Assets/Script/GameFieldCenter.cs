08e65bc1a89aff8d71bc945f46072e80af7e0661
LAPTOP-58QGQQLE\David Private <jonid89@gmail.com>
2023-07-12 23:46:53 +0200

    Shots destroyed on Center

---
+++ Assets/Script/GameFieldCenter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldCenter : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Shot"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
